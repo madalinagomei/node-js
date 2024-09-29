3c566ec44039cfcc9c96f3c04ba114c97953dc26
Madalina Gomei <madalinagomei@gmail.com>
2024-09-29 22:10:25 +0200

    Initial commit

---
+++ cli-contact-app/index.js
@@ -0,0 +1,36 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+const argv = require("yargs").argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const remainingContacts = await removeContact(id);
+      console.table(remainingContacts);
+      break;
+
+    default:
+      console.warn("Unknown action type!");
+  }
+}
+
+invokeAction(argv);
